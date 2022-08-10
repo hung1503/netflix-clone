912fabb1d319e31cdecbdb5324bcb56566b37009
Hung <hungnguyen150300@gmail.com>
2022-08-10 18:53:25 +0300

    Create faqs part

---
+++ src/containers/faqs.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Accordion, OptForm } from "../components";
+import faqsData from "../fixtures/faqs.json";
+
+export function FaqsContainer() {
+  return (
+    <Accordion>
+      <Accordion.Title>Frequently Asked Questions</Accordion.Title>
+      {faqsData.map((faq) => (
+        <Accordion.Item key={faq.id}>
+          <Accordion.Header>{faq.header}</Accordion.Header>
+          <Accordion.Body>{faq.body}</Accordion.Body>
+        </Accordion.Item>
+      ))}
+      <Accordion.Item />
+
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try now</OptForm.Button>
+      </OptForm>
+    </Accordion>
+  );
+}
